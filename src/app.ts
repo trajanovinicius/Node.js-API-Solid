d72b33b8917baeae86c2329c8d2652545a5763b0
Vinicius Trajano <viniciustrajanobs@gmail.com>
2025-06-06 14:51:17 -0300

    feat: create users route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-
-prisma.user.create({
-	data: {
-		name: "Vinicius Trajano",
-		email: "viniciustrajanobs@gmail.com",
-	},
-});
+app.register(appRoutes);
